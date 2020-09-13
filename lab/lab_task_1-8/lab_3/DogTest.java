a58477893eab3e046f8f29204b42a40b276b1387
OrangeCoderRU <max.sablin@mail.ru>
2020-09-13 18:42:04 +0300

    upd

--- lab/lab_task_1-8/lab_3/DogTest.java
+++ lab/lab_task_1-8/lab_3/DogTest.java
@@ -3,11 +3,11 @@ package com.company;
 public class DogTest {
 
     public static void main(String[] args) {
-	Labrador Lina = new Labrador(52, "ретривер", "черный");
+	Labrador Lina = new Labrador(52, "ретривер", "палевый");
 	Lina.displayInfo();
 
 	/*Собака весит: 52.0кг, Принадлежит классу: ретривер
-    Имеет цвет: черный */
+    Имеет цвет: палевый */
 
     }
 }
